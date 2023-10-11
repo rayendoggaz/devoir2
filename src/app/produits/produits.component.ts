c2cda4547b25de216dd41df19ccdc6f8bdd49b61
rayendoggaz <146665409+rayendoggaz@users.noreply.github.com>
2023-10-11 20:00:29 +0100

    first commit

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent {
+  
+
+  produits : Produit[];
+
+  constructor(private produitService: ProduitService ) {
+    
+    this.produits = produitService.listeProduits();
+  }
+}
